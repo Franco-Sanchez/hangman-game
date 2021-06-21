cefb986de30394fbd290ebd1af0dd97b62912bdf
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-06-20 22:56:52 -0500

    Added terminal cleaning and game with attempts

--- start.py
+++ start.py
@@ -1,22 +1,6 @@
 from random import choice
-
-def start_game(word_random, word_to_guess):
-  bad_attempts = 0
-
-  while '-' in word_to_guess:
-    print(word_random)
-    print('¡Adivina la palabra!')
-    print(' '.join(word_to_guess))
-    character = validate_input("Ingresa una letra: ")
-
-    for index, letter in enumerate(word_random):
-      if letter == character:
-        word_to_guess[index] = character
-      else:
-        bad_attempts += 1
-
-  print(f"¡Ganaste! La palabra correcta era {word_random}")
-
+from os import system
+from inflection import pluralize
 
 def fill_data():
   convert = str.maketrans('áéíóú', 'aeiou')
@@ -31,16 +15,47 @@ def validate_input(message):
       character = input(message).upper()
 
       if len(character) == 0:
-        raise ValueError("Tiene que ingresar una letra")
+        raise ValueError("You have to enter a letter")
       elif len(character) > 1:
-        raise ValueError("Debe ingresar solo una letra")
+        raise ValueError("You must enter only one letter")
       elif character.isnumeric(): 
-        raise ValueError("Debe ser una letra, no un número")
+        raise ValueError("It must be a letter, not a number")
       
       return character
 
     except ValueError as ve:
       print(ve)
+      
+
+def start_game(word_random, word_to_guess):
+  attempts = 5
+
+  print("Guess the word!")
+  print(f"You have {attempts} attempts. For every hit you win one more attempt.")
+
+  while '-' in word_to_guess:
+    print(' '.join(word_to_guess))
+    character = validate_input("Enter a letter: ")
+
+    right_guess = False
+
+    for index, letter in enumerate(word_random):
+      if letter == character:
+        word_to_guess[index] = character
+        right_guess = True
+    
+    attempts = attempts + 1 if right_guess else attempts - 1
+    
+    if attempts == 0:
+      system("cls || clear")
+      return print(f"Sorry! The word right was {word_random}.")
+
+    system("cls || clear")
+    print("Guess the word!")
+    print(f"You have {attempts} {pluralize('attempt') if attempts > 1 else 'attempt'} left.")
+
+  system("cls || clear")
+  print(f"Won! The word was {word_random}")
 
 
 def run():
@@ -51,4 +66,5 @@ def run():
 
 
 if __name__ == "__main__":
+  system("cls || clear")
   run()
